5a7cc30281575de319d5517ccc299aafe3c37aac
scottklein7 <scott.klein02@protonmail.com>
2022-02-25 17:38:39 -0500

    Adds submit checkout functionality

--- src/pages/PlaceOrderPage.js
+++ src/pages/PlaceOrderPage.js
@@ -5,8 +5,15 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import FormContainer from '../components/FormContainer'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
 
-function PlaceOrderPage() {
+function PlaceOrderPage({ history }) {
+
+    const orderCreate = useSelector(state => state.orderCreate)
+    const { order, error, success } = orderCreate
+
+    const dispatch = useDispatch()
     const cart = useSelector(state => state.cart)
 
     cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
@@ -14,8 +21,29 @@ function PlaceOrderPage() {
     cart.taxPrice = Number(0.082 * cart.itemsPrice).toFixed(2)
     cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
 
+    if (!cart.paymentMethod) {
+        history.push('/payment')
+    }
+
+    useEffect(() => {
+        if (success) {
+            history.push(`/order/${order._id}`)
+            dispatch({ type: ORDER_CREATE_RESET })
+        }
+
+    }, [success, history])
+
     const placeOrder = () => {
-        console.log('place order')
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+
+        }))
     }
     return (
         <div>
@@ -101,6 +129,10 @@ function PlaceOrderPage() {
                                     <Col>Total: </Col>
                                     <Col>${cart.totalPrice} </Col>
                                 </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                {error && <Message variant='danger'>{error}</Message>}
                             </ListGroup.Item>
 
                             <ListGroup.Item>
